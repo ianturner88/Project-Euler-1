c49dfc2b138bea60818deb25202cf791c41aaf08
ianturner88 <ianturner88@gmail.com>
2017-12-21 15:38:25 -0400

    Update problem-5.cpp

--- solutions/problem-5.cpp
+++ solutions/problem-5.cpp
@@ -1,32 +1,22 @@
-// https://projecteuler.net/problem=5
 #include <iostream>
 using namespace std;
 
-int  main (void)
+int main (void)
 {
-  int small = 1, big = 2, i = 0;
+  int big = 3, little = 2;
   
-  while (small < 21) 
+  while (little < 21)
   {
-    for (small = 2; small < 21; small++)
+    if (big % little == 0)
     {
-      if (big % small == 0)
-      {
-        small++;
-      }
-      
-      if (big % small != 0)
-      {
-        big++;
-        break;
-      }
-      
-      if (big == small)
-      {
-        big++;
-        break;
-      }
+      little ++;
     }
-  }
+  
+    else
+    {
+      big++;
+      little = 2;
+    }
+  } 
   cout << big;
-}  
+}
