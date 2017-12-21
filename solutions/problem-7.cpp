992a6fa262a85cafa44b48579cecbd383793dc6c
ianturner88 <ianturner88@gmail.com>
2017-12-21 15:46:11 -0400

    Update problem-7.cpp

--- solutions/problem-7.cpp
+++ solutions/problem-7.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main(void)
+int main (void)
 {
-  int i, num = 5, count = 2, found;
+  int big = 8, little = 2;
+  int count = 3;
+  int upperlimit = 4;
+  
+  cout << "Please enter nth prime you'd like to find" <<endl;
+  cin >> upperlimit;
 
-  while (count < 100001) {
-  	found = 1; // assume num is prime
-    for (i = 2; i <= (num / 2); i++)
+  while (count < upperlimit - 1)
+  {
+    big++;
+    count++;
+    little = 2;
+    while (little < ((big / 2) + 1))
     {
-      if (num % i == 0) {
-      	  found = 0; // change the flag to zero if num is divisible by a number
-      	  break;
+      if (big % little == 0)
+      {
+        big++;
+        little = 2;
+      }
+      
+      else 
+      {
+        little++;
       }
     }
-    if (found) count++; // increment count if found is 1 (found a prime)
-	num++;
   }
-  cout << count << "th prime is: " << --num;
-  return 0;
+  cout << big;
 }
