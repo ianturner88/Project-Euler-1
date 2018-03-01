b2a5c2702e993007a16d55895501b01cfa1f8ed6
Ian Turner <ianturner88@gmail.com>
2018-03-01 11:19:55 +0100

    Add files via upload

--- solutions/PE179.cpp
+++ solutions/PE179.cpp
@@ -4,31 +4,32 @@ using namespace std;
 
 int main (void)
 {
-	int n = 8;
+	int n = 0;
 	int divisor;
-	int dc, previousdc = 0;
-	int nsqrt;
+	int dc, pdc = 0;
+	float nsqrt;
 	int pc = 0;
 	
-	while (n < pow(10, 7))
+	while (n < 5)
 	{
 		n++;
 		
 		nsqrt = pow(n, 0.5);
 		
-		divisor = 1;
+		divisor = 2;
 		
 		dc = 2;
 		
 		// checks the number of divisors of n
 		while (divisor < nsqrt )
 		{
-			divisor++;
 			
 			if (n % divisor == 0)
 			{
 				dc += 2;
 			}
+			
+			divisor++;
 		}
 		
 		//for numbers like 9, this code double counts the case of 3, so 1 is subtracted. 
@@ -38,13 +39,14 @@ int main (void)
 		}
 		
 		// checks if n & n + 1 have the same number of divisors
-		if (dc == previousdc)
+		if (dc == pdc)
 		{
 			pc++;
+			cout << n << endl;
 		}
 		
 		// stores the previous number's number of divisors!
-		previousdc = dc;
+		pdc = dc;
 	}
 	
 	cout << pc;
