46cb201c05df944de7467425bdd407f1be425ed8
Ian Turner <ianturner88@gmail.com>
2018-03-11 20:54:58 +0100

    Add files via upload

--- solutions/PE008.cpp
+++ solutions/PE008.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int main (void)
 {
 	int test;
+	
+	// determine how often the while loop needs to run
+	
 	cout << "Please enter the number of tests" << endl;
 	cin >> test;
 	
-	int StringLength;
 	int span;
 	string digits;
-	int long long best = 1;
+	int long long best = 0;
 	int long long current = 0;
 	int start = 0;
 	int i;
@@ -21,22 +23,30 @@ int main (void)
 	{
 		i = 1;
 		
+		// the number of digits to be read in
 		cout << "Please enter the string's length: " << endl;
-		cin >> StringLength;
+		cin >> length;
 		
+		//the number of consecutive digits to check
 		cout << "Please enter the span of digits to be tested: " << endl;
 		cin >> span;
 		
+		//the digits themselves
 		cout << "Please enter the digits to be investigated: " << endl;
 		cin >> digits;
 		
+		// start at slot 0 and go until we've come to the last 13 digits.
 		for (start = 0; (start + span) < length; start++)
 		{
+			
+			current = 1;
+			
 			for (i = 0; i < span; i++)
 			{
 				current *=	digits[start + i] - '0';
 			}
 			
+			// update the best 
 			if (current > best)
 			{
 				best = current;
