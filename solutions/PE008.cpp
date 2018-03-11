6ad8023485fafa68e43af5a6991093758b6a29cb
Ian Turner <ianturner88@gmail.com>
2018-03-11 11:55:00 +0100

    Add files via upload

---
+++ solutions/PE008.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (void)
+{
+	int test;
+	cout << "Please enter the number of tests" << endl;
+	cin >> test;
+	
+	int StringLength;
+	int span;
+	string digits;
+	int long long best = 0;
+	int long long current = 0;
+	int start = 0;
+	int i;
+	int length;
+
+	while (test--)
+	{
+		i = 1;
+		
+		cout << "Please enter the string's length: " << endl;
+		cin >> StringLength;
+		
+		cout << "Please enter the span of digits to be tested: " << endl;
+		cin >> span;
+		
+		cout << "Please enter the digits to be investigated: " << endl;
+		cin >> digits;
+		
+		for (start = 0; (start + span) < length; start++)
+		{
+			for (i = 0; i < span; i++)
+			{
+				current *= 	digits[start + i] - '0';
+			}
+			
+			if (current > best)
+			{
+				best = current;
+			}
+		}
+	}
+	
+	cout << best;
+}
