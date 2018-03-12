884393eba3761b2aabe139de93c24994dd15a471
Ian Turner <ianturner88@gmail.com>
2018-03-12 23:00:20 +0100

    Add files via upload

--- solutions/PE008.cpp
+++ solutions/PE008.cpp
@@ -7,13 +7,12 @@ int main (void)
 	int test;
 	
 	// determine how often the while loop needs to run
-	
 	cout << "Please enter the number of tests" << endl;
 	cin >> test;
 	
 	int span;
 	string digits;
-	int long long best = 0;
+	int long long best = 5;
 	int long long current = 0;
 	int start = 0;
 	int i;
@@ -31,16 +30,16 @@ int main (void)
 		cout << "Please enter the span of digits to be tested: " << endl;
 		cin >> span;
 		
-		//the digits themselves
+		//the digits themselves... they can not be entered in line by line but must be read in as a single line
 		cout << "Please enter the digits to be investigated: " << endl;
 		cin >> digits;
 		
 		// start at slot 0 and go until we've come to the last 13 digits.
 		for (start = 0; (start + span) < length; start++)
 		{
-			
 			current = 1;
 			
+			//convert the digits from ASCII to numbers
 			for (i = 0; i < span; i++)
 			{
 				current *=	digits[start + i] - '0';
