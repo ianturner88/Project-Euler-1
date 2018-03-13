b590edb1adf179b23b8c20aab71eaefda5ad7f10
Ian Turner <ianturner88@gmail.com>
2018-03-13 23:17:30 +0100

    Add files via upload

--- solutions/PE031.cpp
+++ solutions/PE031.cpp
@@ -1,4 +1,4 @@
-// 1 pence, 2 pence, 5 pence, 10 pence, 20 pence, 50 pence, 1 pound, 2 pound
+// 1 pence, 0.01; 2 pence, 0.02; 5 pence, 0.05; 10 pence, 0.1; 20 pence, 0.2; 50 pence, 0.5; 1 pound, 1; 2 pound, 2
 
 #include <iostream>
 using namespace std;
@@ -7,56 +7,38 @@ int main()
 {
 	int count = 0;
 	int sum = 0;
-	int x;
+	int x, y, z, w, v, u, t, s;
 	
-	while (sum < 2)
+	for (x = 0; x < 200; x += 1)
 	{
-		while (sum < 2)
+		for (y = 0; y < 100; y += 2)
 		{
-			x = 0.01;
-			
-			sum += x;
-			
-			if (sum == 2)
+			for (z = 0; z < 40; z += 5)
 			{
-				count++;
-			}
-			
-			while (sum < 2)
-			{
-				x = 0.02;
-				
-				sum += x;
-				
-				if (sum == 2)
+				for (w = 0; w < 20; z += 20)
 				{
-					count++;
-				}
-				
-				while (sum < 2)
-				{
-					x = 0.05;
-					
-					sum += x;
-					
-					if (sum == 2)
+					for (v = 0; v < 10; v += 10)
 					{
-						count++;
-					}
-					
-					while (sum < 2)
-					{
-						x = 0.1;
-						
-						sum += x;
-						
-						if (sum == 2)
+						for (u = 0; u < 4; u += 4)
 						{
-							count++;
+							for (t = 0; t < 2; t += 2)
+							{
+								for (s = 0; s < 1; s += 1)
+								{
+									sum = x + y + z + w + v + u + t + s;
+									
+									if (sum == 2)
+									{
+										count++;
+									}
+								}
+							}
 						}
 					}
 				}
 			}
 		}
 	}
+	
+	cout << count;
 }
