94ed90a428a15b3c706777188b4ac3ce8a0424a4
Ian Turner <ianturner88@gmail.com>
2018-03-14 23:03:23 +0100

    Add files via upload

--- solutions/PE031.cpp
+++ solutions/PE031.cpp
@@ -9,25 +9,25 @@ int main()
 	int sum = 0;
 	int x, y, z, w, v, u, t, s;
 	
-	for (x = 0; x < 200; x += 1)
+	for (x = 0; x < 2; x += 1)
 	{
-		for (y = 0; y < 100; y += 2)
+		for (y = 0; y < 3; y += 2)
 		{
-			for (z = 0; z < 40; z += 5)
+			for (z = 0; z < 5; z += 5)
 			{
-				for (w = 0; w < 20; z += 20)
+				for (w = 0; w < 11; z += 20)
 				{
-					for (v = 0; v < 10; v += 10)
+					for (v = 0; v < 21; v += 10)
 					{
-						for (u = 0; u < 4; u += 4)
+						for (u = 0; u < 41; u += 4)
 						{
-							for (t = 0; t < 2; t += 2)
+							for (t = 0; t < 101; t += 2)
 							{
-								for (s = 0; s < 1; s += 1)
+								for (s = 0; s < 201; s += 1)
 								{
 									sum = x + y + z + w + v + u + t + s;
 									
-									if (sum == 2)
+									if (sum == 200)
 									{
 										count++;
 									}
