a866549ff11178835c0858a15ea426a674b4ff8a
Ian Turner <ianturner88@gmail.com>
2018-03-16 08:00:33 +0100

    Add files via upload

--- solutions/PE031.cpp
+++ solutions/PE031.cpp
@@ -11,48 +11,48 @@ int main()
 	int upperlimit = 201;
 	
 	// 2 pound = 200
-	for (x = 0; x < upperlimit && sum < upperlimit; x += 200)
+	for (x = 0; x < 201; x += 200)
 	{
-		sum = x;
+		//sum = x;
 		
 		// 1 pound = 100
-		for (y = 0; y < upperlimit && sum < upperlimit; y += 100)
+		for (y = 0; y < 301; y += 100)
 		{
 			
-			sum = x + y;
+			//sum = x + y;
 			
 			// 50 pence = 50
-			for (z = 0; z < upperlimit && sum < upperlimit; z += 5)
+			for (z = 0; z < 51; z += 5)
 			{
 				
-				sum = x + y + z;
+				//sum = x + y + z;
 				
 				// 20 pence = 20
-				for (w = 0; w < upperlimit && sum < upperlimit; w += 20)
+				for (w = 0; w < 101; w += 20)
 				{
 					
-					sum = x + y + z + w;
+					//sum = x + y + z + w;
 					
 					// 10 pence = 10
-					for (v = 0; v < upperlimit && sum < upperlimit; v += 10)
+					for (v = 0; v < 21; v += 10)
 					{
 						
-						sum = x + y + z + w + v;
+						//sum = x + y + z + w + v;
 						
 						// 5 pence = 5
-						for (u = 0; u < upperlimit && sum < upperlimit; u += 5)
+						for (u = 0; u < 41; u += 5)
 						{
 							
-							sum = x + y + z + w + u;
+							//sum = x + y + z + w + u;
 							
 							// 2 pence = 2
-							for (t = 0; t < upperlimit && sum < upperlimit; t += 2)
+							for (t = 0; t < 101; t += 2)
 							{
 								
-								sum = x + y + z + w + u + t;
+								//sum = x + y + z + w + u + t;
 								
 								// 1 pence = 1
-								for (s = 0; s < upperlimit && sum < upperlimit; s += 1)
+								for (s = 0; s < 201; s += 1)
 								{
 									sum = x + y + z + w + v + u + t + s;
 									
