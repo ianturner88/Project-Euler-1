328eea91844a05b534050c86121e543837499856
Ian Turner <ianturner88@gmail.com>
2018-04-02 17:34:02 +0200

    Add files via upload

--- solutions/PE001.cpp
+++ solutions/PE001.cpp
@@ -1,31 +1,55 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main (void)
+void sum3(int &x, int &u);
+void sum5(int &y, int &u);
+void sum15(int &z, int &u);
+void sumlimit(int &x, int &y, int &z, int &s);
+
+int main ()
 {
-  int x,y,z;
-  int sum=0, sum15=0;
-  int upperlimit;
-  
-  cout << "Please enter an upperlimit limit" << endl;
-  cin >> upperlimit;
-  
-  for (x=0; x<upperlimit; x+=3)
-  {
-    sum += x;
-  }
-  
-  for (y=0; y<upperlimit; y+=5)
-  {
-    sum += y;
-  }
-  
-  for (z=0; z<upperlimit; z+=15)
-  {
-    sum15 += z;
-  }
-  
-  sum = sum - sum15;
-  
-  cout << sum;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int upperlimit = 1000;
+	int sum = 0;
+	
+	sum3(a, upperlimit);
+	sum5(b, upperlimit);
+	sum15(c, upperlimit);
+	sumlimit(a, b, c, sum);
+	
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << sum;
+}
+
+void sum3(int &x, int &u)
+{
+	for (int m = 0; m < u; m += 3)
+	{
+		x += m;
+	}
+}
+
+void sum5(int &y, int &u)
+{
+	for (int m = 0; m < u; m += 5)
+	{
+		y += m;
+	}
+}
+
+void sum15(int &z, int &u)
+{
+	for (int m = 0; m < u; m += 15)
+	{
+		z += m;
+	}
+}
+
+void sumlimit(int &x, int &y, int &z, int &s)
+{
+	s = x + y - z;
 }
