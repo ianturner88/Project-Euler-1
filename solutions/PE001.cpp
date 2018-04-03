620deecf4be790447e362f67b167bcafbae9aed7
Ian Turner <ianturner88@gmail.com>
2018-04-03 18:33:55 +0200

    Add files via upload

--- solutions/PE001.cpp
+++ solutions/PE001.cpp
@@ -1,51 +1,53 @@
 #include<iostream>
 using namespace std;
 
-void sum3(int &x, int &u);
-void sum5(int &y, int &u);
-void sum15(int &z, int &u);
+void calcsum(int &x, int &u, int &h);
 void sumlimit(int &x, int &y, int &z, int &s);
+void answer(int &a, int &b, int &c, int &s);
+void upperlimit(int &u);
 
 int main ()
 {
 	int a = 0;
 	int b = 0;
 	int c = 0;
-	int upperlimit = 1000;
+	int ul = 0;
 	int sum = 0;
 	
-	sum3(a, upperlimit);
-	sum5(b, upperlimit);
-	sum15(c, upperlimit);
-	sumlimit(a, b, c, sum);
+	upperlimit(ul);
 	
+	int h = 3;
+	calcsum(a, ul, h);
+	
+	h = 5;
+	calcsum(b, ul, h);
+	
+	h = 15;
+	calcsum(c, ul, h);
+	
+	sumlimit(a, b, c, sum);
+	answer(a, b, c, sum);
+}
+
+void upperlimit(int &u)
+{
+	cout << "Please enter the upperlimit" << endl;
+	cin >> u;
+}
+
+void answer(int &a, int &b, int &c, int &s)
+{
 	cout << a << endl;
 	cout << b << endl;
 	cout << c << endl;
-	cout << sum;
+	cout << s;
 }
 
-void sum3(int &x, int &u)
+void calcsum(int &x, int &u, int &h)
 {
-	for (int m = 0; m < u; m += 3)
+	for (int m = 0; m < u; m += h)
 	{
 		x += m;
-	}
-}
-
-void sum5(int &y, int &u)
-{
-	for (int m = 0; m < u; m += 5)
-	{
-		y += m;
-	}
-}
-
-void sum15(int &z, int &u)
-{
-	for (int m = 0; m < u; m += 15)
-	{
-		z += m;
 	}
 }
 
