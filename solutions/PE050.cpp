5bc371c198cb5959a4a6f422090a41b55d333a08
Ian Turner <ianturner88@gmail.com>
2018-04-05 23:42:04 +0200

    Add files via upload

--- solutions/PE050.cpp
+++ solutions/PE050.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main (void)
 {
-	long long int n = 50;
+	long long int n = 1000;
 	long long int a = 0;
 	
 	bool prime[n + 1];
@@ -15,42 +15,37 @@ int main (void)
 	long long int possiblecount = 0;
 	long long int answercount = 1;
 	
-	while (a < 50)
+	for(long long int p = 2; p*p <= n; p++)
 	{
-		for(long long int p = 2; p*p <= n; p++)
-		{
-			for(long long int i = 2*p; i <= n; i += p)
+		for(long long int i = 2*p; i <= n; i += p)
 			{
 				prime[i] = false;
 			}
-		}
+	}
 	
-		for(long long int c = 2; c <= n; c++)
+	for(long long int c = 2; c <= n; c++)
+	{
+		if (prime[c] == true)
 		{
-			if (prime[c] == true)
-			{
-				sum = 0;
-				count = 0;
+			sum = 0;
+			count = 0;
 				
-				for (long long int b = c; b <= n; b++)
+			for (long long int b = c; b <= n; b++)
+			{
+				if (prime[b] == true)
 				{
-					if (prime[b] == true)
-					{
-						sum += b;
-						count++;
-					}
+					sum += b;
+					count++;
+				}
 					
-					if (prime[sum] == true && prime[b] == true && sum < n && count > answercount)
-					{
-						possiblesum = sum;
-						answercount = count;
-					}
+				if (prime[sum] == true && prime[b] == true && sum < n && count > answercount)
+				{
+					possiblesum = sum;
+					answercount = count;
 				}
 			}
 		}
-		
-		a = 51;
 	}
 	
 	cout << possiblesum << ": " << answercount;
-}	
+}
