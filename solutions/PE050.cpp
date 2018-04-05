d27aeabbfa97bf86aa542c8992861031ac97c187
Ian Turner <ianturner88@gmail.com>
2018-04-05 21:55:41 +0200

    Add files via upload

---
+++ solutions/PE050.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Eratosthenes(int n);
+
+int main (void)
+{
+	long long int n = 1000;
+	
+	cout << "The answer is: " << endl;
+	
+	Eratosthenes(n);
+}
+
+void Eratosthenes(int n)
+{
+	bool prime[n + 1];
+	memset(prime, true, sizeof(prime));
+	long long int sum = 0;
+	long long int count = 0;
+	long long int possiblesum = 0;
+	long long int answersum = 1;
+	long long int possiblecount = 0;
+	long long int answercount = 1;
+	
+	for(long long int p = 2; p*p <= n; p++)
+	{
+		for(long long int i = 2*p; i <= n; i += p)
+		{
+			prime[i] = false;
+		}
+	}
+	
+	for(long long int p = 2; p <= n; p++)
+	{
+		if (prime[p] == true)
+		{
+			sum += p;
+			count++;
+		}
+		
+		if (prime[sum] == true && prime[p] == true && sum < n)
+		{
+			possiblesum = sum;
+			answercount = count;
+		}
+	}
+	
+	cout << possiblesum << ": " << answercount;
+}
