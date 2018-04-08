1466d277b7bde3e91f0afe2bf2dff04d4684080a
Ian Turner <ianturner88@gmail.com>
2018-04-08 08:21:44 +0200

    Add files via upload

--- solutions/PE Lucky Clover.cpp
+++ solutions/PE Lucky Clover.cpp
@@ -1,3 +1,27 @@
+/* 
+Consider the first three prime numbers: 2, 3, and 5. If we mutliply
+3 with 5, we get 15, which is not prime. Yet, if we subtract 2 from 15, 
+we get 13, which is prime.
+
+This case is the only one where when one multiplies two primes together
+and subtracts a third results in a prime. This is because, when two odd-
+numbered primes are multiplied together, the result is odd, and by 
+subtracting an third odd-numbered prime, the result will be even.
+
+Now, let's consider the 4 consecutive primes 5, 7, 11, and 13.
+
+If we mutliply 11 and 13 together, we get 143. If now we subtract
+5 and 7 from 143, we get 131 - which is prime.
+
+Let us call 5, 7, 11, and 13 a prime four-leaf clover.
+
+How many prime four-leaf clover are there under 1 000 000?
+
+Note: Some sets of four may contain some but not all of the 
+same elements as other sets. 
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
