cf5e52e0fdc795d2db353266703d35c27bcec89a
Ian Turner <ianturner88@gmail.com>
2018-04-15 21:54:26 +0200

    Add files via upload

--- solutions/Reference.cpp
+++ solutions/Reference.cpp
@@ -6,12 +6,13 @@ int main (void)
 	int number = 88; //an integer variable called number gets the value 88
 	int &alias = number; // the integer variable alias is a duplicate of the integer variable number. Number & alias are linked; they will always have the same value
 	
-	cout << alias; // prints 88
+	cout << alias << endl; // prints 88
+	cout << number; // prints 88
 	
 	cout << endl; // space
 	
-	alias = 77; // both alias 
+	alias = 77; // both alias & number are changed to 77
 	
-	cout << alias << endl;
-	cout << number;
+	cout << alias << endl; // 77 is printed
+	cout << number; // 77 is printed
 }
