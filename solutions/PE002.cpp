d29315f3a982884ec602e34ae347db10ccce4ea6
Ian Turner <ianturner88@gmail.com>
2018-04-17 22:17:47 +0200

    Add files via upload

--- solutions/PE002.cpp
+++ solutions/PE002.cpp
@@ -1,24 +1,52 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include <math.h>
 using namespace std;
+
+void Fibonacci(bool prime[], int);
+void Answer(bool prime[], int n);
 
 int main (void)
 {
-  int F1 = 1;
-  int F2 = 1;
-  int Fnext = 0;
-  int sum = 0;
-  
-  while (F1<4000000)
-  {
-    Fnext = F1 + F2;
-    F1 = F2;
-    F2 = Fnext;
-    
-    if (Fnext % 2 == 0)
-    {
-      sum += Fnext;
-    }
-  }
-  
-  cout << sum;
+	long long int n = 100;
+	long long int sum = 0;
+	long long int p;
+	
+	bool prime[n + 1];
+	memset(prime, false, sizeof(prime));
+	
+	Fibonacci(prime, n);
+	Answer(prime, n);
+	
+}
+
+void Fibonacci(bool prime[], int n)
+{
+	int Fnext, F1 = 1, F2 = 1;
+	
+	while (F1 < n)
+	{
+		Fnext = F1 + F2;
+		F1 = F2;
+		F2 = Fnext;
+		
+		if (Fnext % 2 == 0)
+		{
+			prime[Fnext] = true;
+		}
+	}
+}
+
+void Answer(bool prime[], int n)
+{
+	int sum = 0;
+	
+	for (int p = 0; p < n; p++)
+	{
+		if (prime[p] == true)
+		{
+			sum += p;	
+		}
+	}
+	
+	cout << sum;
 }
