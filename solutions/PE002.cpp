8ca69938d21280f41933517b5f8e80d5c24bea33
Ian Turner <ianturner88@gmail.com>
2018-04-18 11:31:10 +0200

    Add files via upload

--- solutions/PE002.cpp
+++ solutions/PE002.cpp
@@ -7,7 +7,7 @@ void Answer(bool prime[], int n);
 
 int main (void)
 {
-	long long int n = 100;
+	long long int n = 1000000;
 	
 	bool prime[n + 1];
 	memset(prime, false, sizeof(prime));
@@ -18,26 +18,32 @@ int main (void)
 
 void Fibonacci(bool prime[], int n)
 {
-	int Fnext, F1 = 1, F2 = 1;
+	long long int Fnext, F1 = 1, F2 = 1;
+	int flag = 0;
 	
-	while (F1 < n)
+	while (flag == 0)
 	{
+		if (Fnext % 2 == 0)
+		{
+			prime[Fnext] = true;
+		}
+		
 		Fnext = F1 + F2;
 		F1 = F2;
 		F2 = Fnext;
 		
-		if (Fnext % 2 == 0)
+		if (Fnext > n || F1 > n || F2 > n)
 		{
-			prime[Fnext] = true;
+			flag = 1;
 		}
 	}
 }
 
 void Answer(bool prime[], int n)
 {
-	int sum = 0;
+	long long int sum = 0;
 	
-	for (int p = 0; p < n; p++)
+	for (long long int p = 0; p < n; p++)
 	{
 		if (prime[p] == true)
 		{
