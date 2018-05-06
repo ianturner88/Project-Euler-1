bf7f5906a03cf6e4db051efaa4ea7de93a606fff
Ian Turner <ianturner88@gmail.com>
2018-05-06 14:28:43 -0300

    Add files via upload

--- solutions/Dynamic Array.cpp
+++ solutions/Dynamic Array.cpp
@@ -5,9 +5,11 @@ const int CAPACITY = 100000;
 
 int main (void)
 {
+	// makes a pointer to the array & states the size of the array
 	int *dArray;
 	dArray = new int[CAPACITY];
 	
+	// states the elements of each slot
 	dArray[0] = 1;
 	dArray[1] = 3;
 	dArray[2] = 5;
@@ -15,8 +17,16 @@ int main (void)
 	
 	cout << "Array: ";
 	
+	// prints the contents of the array
 	for (int i = 0; i < 4; i++)
 	{
 		cout << dArray[i] << " ";
 	}
+	
+	//deletes the array 
+	delete [] dArray;
+	
+	cout << " The last call: " << dArray[0];
+	
+	dArray = NULL;
 }
