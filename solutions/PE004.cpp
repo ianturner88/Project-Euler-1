259a45ccab0b569d881d7834faf6f4c0e0b92332
Ian Turner <ianturner88@gmail.com>
2018-06-07 23:51:16 -0300

    Add files via upload

--- solutions/PE004.cpp
+++ solutions/PE004.cpp
@@ -1,4 +1,5 @@
-/* Question: A palindromic number reads the same both ways. The largest 
+/* 
+Question: A palindromic number reads the same both ways. The largest 
 palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99. 
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
@@ -7,26 +8,36 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 using namespace std;
 
 int Palindrome(int answer);
-void Divisor_First(int &answer, int &flag);
-int Divisor_Second(int first_divisor);
+void Divisor_First(int testcase, int &flag, int &first_divisor, int &second_divisor);
+void Condition_Check(int testcase, int &flag, int &first_divisor, int &second_divisor);
 
 int main(void)
 {
 	int flag = 0;
-	int answer = 987;
+	int answer = 999;
+	int first_divisor = 100;
+	int second_divisor = 0;
+
+	/*The flag will be 'put up & stay up' if a first & second divisor are found
+	that are each three digits long*/
 
 	while (flag == 0)
 	{
 		Palindrome(answer);
-		if (Divisor_First(flag) == 1)
+		Divisor_First(Palindrome(answer), flag, first_divisor, second_divisor);
+		if (flag == 1)
 		{
-			Divisor_Second();
+			Condition_Check(Palindrome(answer), flag, first_divisor, second_divisor);
 		}
+
+		cout << Palindrome(answer);
 	}
 }
 
 int Palindrome(int answer)
 {
+	/*Produce the palindromes*/
+	
 	int testcase;
 	
 	answer--;
@@ -36,31 +47,54 @@ int Palindrome(int answer)
 	return testcase;
 }
 
-void Divisor_First(int &answer, int &flag)
+void Divisor_First(int testcase, int &flag, int &first_divisor, int &second_divisor)
 {
-	int divisor = 100;
-	int testcase = answer;
-
-	while (divisor < (testcase / 2) && flag == 0)
+	while (first_divisor < (testcase / 2) && flag == 0)
 	{
-		if (testcase % divisor == 0)
+		/*If the first divisor of the palindrome is found, the second is 
+		also recored. Otherwise, increment the variable 'first_divisor' to 
+		see if the variable divides into the palindrome.*/
+		
+		if (testcase % first_divisor == 0)
 		{
 			flag = 1;
+			second_divisor = testcase / first_divisor;
 		}
 
 		else
 		{
-			divisor++;
+			first_divisor++;
 		}
 	}
 }
 
-int Divisor_Second(int first_divisor)
+void Condition_Check(int testcase, int &flag, int &first_divisor, int &second_divisor)
 {
-	int second_divisor = testcase / first_divisor;
+	/*Ensure that both divisors are 3 digits long.*/
+	
+	flag = 0;
+	int count_first = 0;
+	int count_second = 0;
 
-	if (sizeof(second_divisor) == 3)
+	while (first_divisor > 0)
 	{
+		first_divisor = first_divisor / 10;
+		count_first++;
+	}
 
+	while (second_divisor > 0)
+	{
+		second_divisor = second_divisor / 10;
+		count_second++;
+	}
+
+	if (count_first == 3 && count_second == 3)
+	{
+		flag = 1;
+	}
+
+	else
+	{
+		flag = 0;
 	}
 }
