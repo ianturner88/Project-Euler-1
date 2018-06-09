f897d056a9c9eaebe5cc19dd4f8ec97335143338
Ian Turner <ianturner88@gmail.com>
2018-06-09 16:59:19 -0300

    Add files via upload

--- solutions/PE034.cpp
+++ solutions/PE034.cpp
@@ -1,46 +1,70 @@
-#include <iostream>
-#include <math.h>
-
+#include<iostream>
 using namespace std;
 
-int main (void)
+void Factorials(const int &n, int factorial_sum[]);
+void Digit_Factorials_Sum(int &testcase, int factorial_sum[], int &sum);
+
+int main(void)
 {
-	int n = 2;
-	int r;
-	int rsum;
-	int answer = 0;
-	int sum;
-	int dummynumber;
+	const int n = 10;
+	int testcase = 2;
+	int upperlimit = 2540160;
+	int sum = 0;
 	
-	while (n < pow(10, 7))
+	int factorial_sum[n];
+
+	std::fill_n(factorial_sum, n, 1); // set all the values of the array equal to 1
+
+	Factorials(n, factorial_sum);
+
+	while (testcase < upperlimit)
 	{
-		n++;
-		rsum = 1;
-		sum = 0;
-		dummynumber = n;
-		
-		while(dummynumber > 0)
+		testcase++;
+		Digit_Factorials_Sum(testcase, factorial_sum, sum);
+	}
+
+	cout << sum << endl;
+
+	system("pause");
+}
+
+void Factorials(const int &n, int factorial_sum[])
+{
+	/*Calculates and stores the values of the factorials
+	of 1 through 9*/
+	
+	for (int i = 1; i < n; i++)
+	{
+		int remainder = i;
+
+		while (remainder > 0)
 		{
-			r = dummynumber % 10;
-			dummynumber = dummynumber / 10;
-			rsum = 1;
-			
-			while (r > 0)
-			{
-				rsum *= r;
-				
-				r--; 
-			}
-			
-			sum += rsum;	
-		}
-			
-		if (sum == n)
-		{
-			answer += n;
-			cout << n << endl;
+			factorial_sum[i] *= remainder;
+
+			remainder--;
 		}
 	}
+}
+
+void Digit_Factorials_Sum(int &testcase, int factorial_sum[], int &sum)
+{
+	/*Check if the sum of the given number's digits' 
+	factorial is equal to the number itself*/
 	
-	cout << "Answer: " << answer;
+	int  number = testcase;
+	int remainder;
+	int remainder_digit_factorial_sum = 0;
+
+	while (number > 0)
+	{
+		remainder = number % 10;
+		remainder_digit_factorial_sum += factorial_sum[remainder];
+
+		number /= 10;
+	}
+
+	if (remainder_digit_factorial_sum == testcase)
+	{
+		sum += testcase;
+	}
 }
