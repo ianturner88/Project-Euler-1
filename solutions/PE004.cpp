8a72a84a955163bc1bac759a257354d7a775cadc
Ian Turner <ianturner88@gmail.com>
2018-06-09 16:13:06 -0300

    Add files via upload

--- solutions/PE004.cpp
+++ solutions/PE004.cpp
@@ -7,48 +7,53 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 #include<iostream>
 using namespace std;
 
-int Palindrome(int answer);
-void Divisor_First(int testcase, int &flag, int &first_divisor, int &second_divisor);
-void Condition_Check(int testcase, int &flag, int &first_divisor, int &second_divisor);
+void Palindrome(int &answer, int &testcase);
+void Divisor_First(int &testcase, int &flag, int &first_divisor, int &second_divisor);
+void Condition_Check(int &testcase, int &flag, int &first_divisor, int &second_divisor);
 
 int main(void)
 {
 	int flag = 0;
-	int answer = 999;
+	int answer = 954;
 	int first_divisor = 100;
 	int second_divisor = 0;
+	int testcase = 0;
 
 	/*The flag will be 'put up & stay up' if a first & second divisor are found
 	that are each three digits long*/
 
 	while (flag == 0)
 	{
-		Palindrome(answer);
-		Divisor_First(Palindrome(answer), flag, first_divisor, second_divisor);
+		first_divisor = 100;
+		second_divisor = 0;
+
+		Palindrome(answer, testcase);
+		Divisor_First(testcase, flag, first_divisor, second_divisor);
 		if (flag == 1)
 		{
-			Condition_Check(Palindrome(answer), flag, first_divisor, second_divisor);
+			Condition_Check(testcase, flag, first_divisor, second_divisor);
 		}
-
-		cout << Palindrome(answer);
 	}
+
+	cout << answer << endl;
+	cout << (answer / 100) * 1000 + ((answer / 10) % 10) * 10000 + ((answer % 10) * 100000) + answer << endl;
+
+	system("pause");
 }
 
-int Palindrome(int answer)
+void Palindrome(int &answer, int &testcase)
 {
 	/*Produce the palindromes*/
-	
-	int testcase;
 	
 	answer--;
 
 	testcase = (answer / 100) * 1000 + ((answer / 10) % 10) * 10000 + ((answer % 10) * 100000) + answer;
-
-	return testcase;
 }
 
-void Divisor_First(int testcase, int &flag, int &first_divisor, int &second_divisor)
+void Divisor_First(int &testcase, int &flag, int &first_divisor, int &second_divisor)
 {
+	//first_divisor = 100;
+	
 	while (first_divisor < (testcase / 2) && flag == 0)
 	{
 		/*If the first divisor of the palindrome is found, the second is 
@@ -68,13 +73,14 @@ void Divisor_First(int testcase, int &flag, int &first_divisor, int &second_divi
 	}
 }
 
-void Condition_Check(int testcase, int &flag, int &first_divisor, int &second_divisor)
+void Condition_Check(int &testcase, int &flag, int &first_divisor, int &second_divisor)
 {
 	/*Ensure that both divisors are 3 digits long.*/
 	
 	flag = 0;
 	int count_first = 0;
 	int count_second = 0;
+	int count_testcase = 0;
 
 	while (first_divisor > 0)
 	{
@@ -88,7 +94,13 @@ void Condition_Check(int testcase, int &flag, int &first_divisor, int &second_di
 		count_second++;
 	}
 
-	if (count_first == 3 && count_second == 3)
+	while (testcase > 0)
+	{
+		testcase = testcase / 10;
+		count_testcase++;
+	}
+
+	if (count_first == 3 && count_second == 3 && count_testcase == 6)
 	{
 		flag = 1;
 	}
