724199e6091ed9fb13f22355b007e796f57aa90a
Ian Turner <ianturner88@gmail.com>
2018-06-11 16:08:07 -0300

    Add files via upload

--- solutions/PE035.cpp
+++ solutions/PE035.cpp
@@ -1,79 +1,102 @@
-#include <iostream>
-#include <math.h>
+/* 
+RTF:
+The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+How many circular primes are there below one million?
+*/
+
+#include<iostream>
 using namespace std;
 
-int main (void)
-{
-  int prime = 0;
-  int small = 1, big = 8, flag = 0;
-  int dummyprime = 0, count = 0;
-  int circularprime = 0, remainderprime = 0, chopper = 0;
-  int numbercp = 4;
-  int sqrtlimit = 0;
-  int primecheck = 0;
-  int circularcount = 0;
+void Is_Prime(bool prime[], const int &upperlimit);
+void Is_Circular(bool prime[], const int &upperlimit, int &n, int &answer);
 
-  while (big < 1000000)
-  {
-    big++;
-    flag = 1;
-    sqrtlimit = sqrt(big);
-    primecheck = 0;
-    small = 1;
-    count = 0;
-    
-    while (small < sqrtlimit && flag != 0)
-    {
-      small++;
-	  if (big % small == 0)
-      {
-        flag = 0;
-      }
-    }
-    
-    if (flag == 1)
-    {
-      prime = big;
-      dummyprime = prime;
-      primecheck = 1;
-    }
-    
-    while (dummyprime != 0)
-    {
-      dummyprime = dummyprime/10;
-      count++;
-    }
-    
-    circularprime = prime;
-    flag = 1;
-    circularcount = count;
-    count = count - 1;
-    
-    while (circularcount > 1 && primecheck == 1 && flag != 0)
-    {
-	  circularcount--;
-	  remainderprime = circularprime%10;
-      chopper = circularprime/10;
-      circularprime = remainderprime*pow(10,count) + chopper;
-      
-      sqrtlimit = sqrt(circularprime);
-      small = 1;
-      
-      while (small < sqrtlimit && flag != 0)
-      {
-        small++;
-		if (circularprime%small == 0)
-        {
-          flag = 0;
-        }
-      }
-    }
-    
-    if (flag == 1 && primecheck == 1)
-    {
-      numbercp++;
-    }
-  }
-  
-  cout << numbercp;
+int main(void)
+{
+	/*Initialize the upperlimit; the testcase, n; the boolean array*/
+	
+	const int upperlimit = 1000000;
+	int n = 1;
+	int answer = 0;
+
+	bool prime[upperlimit];
+	memset(prime, true, sizeof(prime));
+
+	/*Identify all primes below 1 000 000*/
+	Is_Prime(prime, upperlimit);
+
+	/*if n is prime, the algorithm in the function Is_Circular
+	checks if the prime is also circularily prime*/
+	while (n < upperlimit)
+	{
+		n += 2;
+		
+		if (prime[n] == true)
+		{
+			Is_Circular(prime, upperlimit, n, answer);
+		}
+	}
+
+	cout << answer + 1 << endl;
+
+	system("pause");
+}
+
+void Is_Prime(bool prime[], const int &upperlimit)
+{
+	/*Identify all primes below 1 000 000*/
+	
+	for (int p = 2; p <= upperlimit; p++)
+	{
+		for (int i = 2 * p; i <= upperlimit; i += p)
+		{
+			prime[i] = false;
+		}
+	}
+}
+
+void Is_Circular(bool prime[], const int &upperlimit, int &n, int &answer)
+{
+	/*Check if the initial prime is also circularily prime*/
+	
+	int test_number = n;
+	int count = 0;
+	int flag = 1;
+	int remainder;
+	int exponent = count;
+
+	/*Determine the number's length*/
+	while (test_number > 0)
+	{
+		test_number = test_number / 10;
+		count++;
+	}
+
+	test_number = n;
+	exponent = count - 1;
+	
+	if (count > 1)
+	{
+		count--;
+	}
+
+	/*Check if the prime is circularily prime*/
+	while (count > 0 && flag == 1)
+	{
+		remainder = test_number % 10;
+		test_number = test_number / 10;
+		test_number = test_number + remainder * pow(10, exponent);
+
+		if (prime[test_number] == false)
+		{
+			flag = 0;
+		}
+
+		count--;
+
+		if (count == 0 && flag != 0)
+		{
+			answer++;
+		}
+	}
 }
