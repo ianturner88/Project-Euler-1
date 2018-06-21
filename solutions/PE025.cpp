d57eabfa81f91880ee6f99ea5fa835d9c060f8e9
Ian Turner <ianturner88@gmail.com>
2018-06-20 23:24:56 -0300

    Add files via upload

--- solutions/PE025.cpp
+++ solutions/PE025.cpp
@@ -1,35 +1,58 @@
-#include <iostream>
-#include <math.h>
-
+#include<string>
+#include<iostream>
 using namespace std;
+
+void Length_Calculator(int &n1, int &n2, int &length_difference, string &fibonacci1, string &fibonacci2);
+void Fibonacci_Generator(string &fibonacci1, string &fibonacci2, string &fibonacci_next);
+void Add_LSB(string &fibonacci1, string &fibonacci2, string &fibonacci_next, int &length_difference, int &carry, int &n1, int &n2, int &sum);
+void Add_MSB(string &fibonacci1, string &fibonacci2, string &fibonacci_next, int &length_difference, int &carry, int &n1, int &n2, int &sum);
 
 int main()
 {
-	long long int F1 = 1, F2 = 1, Fnext;
-	long int flag = 0;
-	int a = 999;
-	
-	while (flag != 1)
+	string fibonacci1 = "1", fibonacci2 = "1", fibonacci_next = "2";
+	int length_difference, carry;
+	int n1, n2, sum;
+
+	Length_Calculator(n1, n2, length_difference, fibonacci1, fibonacci2);
+	Fibonacci_Generator(fibonacci1, fibonacci2, fibonacci_next);
+	Add_LSB(fibonacci1, fibonacci2, fibonacci_next, length_difference, carry, n1, n2, sum);
+	Add_MSB(fibonacci1, fibonacci2, fibonacci_next, length_difference, carry, n1, n2, sum);
+}
+
+void Length_Calculator(int &n1, int &n2, int &length_difference, string &fibonacci1, string &fibonacci2)
+{
+	n1 = fibonacci1.length();
+	n2 = fibonacci2.length();
+
+	length_difference = n2 - n1;
+}
+
+void Fibonacci_Generator(string &fibonacci1, string &fibonacci2, string &fibonacci_next)
+{
+	/*Traditionally, Fnext = F1 + F2 is the first line*/
+
+	fibonacci1 = fibonacci2;
+	fibonacci2 = fibonacci_next;
+}
+
+void Add_LSB(string &fibonacci1, string &fibonacci2, string &fibonacci_next, int &length_difference, int &carry, int &n1, int &n2, int &sum)
+{
+	/*Add the least significant bits of both numbers*/
+
+	for (int i = n1 - 1; i >= 0; i--)
 	{
-		Fnext = F1 + F2;
-		F1 = F2 + Fnext;
-		F2 = Fnext + F1;
-		
-		if (Fnext > pow(10,a))
-		{
-			flag = 1;
-		}
-		
-		if (F1 > pow(10,a))
-		{
-			flag = 1;
-		}
-		
-		if (F2 > pow(10,a))
-		{
-			flag = 1;
-		}
+		sum = (fibonacci1[i] - '0') + (fibonacci2[i + length_difference] - '0') + carry;
+
+		fibonacci_next.push_back(sum % 10 + '0');
+
+		carry = sum / 10;
 	}
-	
-	cout << F1 << " " << F2 << " " << Fnext;
+}
+
+void Add_MSB(string &fibonacci1, string &fibonacci2, string &fibonacci_next, int &length_difference, int &carry, int &n1, int &n2, int &sum)
+{
+	for (int i = n2 - n1 - 1; i <= 0; i--)
+	{
+
+	}
 }
