b93ae777bbc67496d3581248c0716a7cf526d0f6
Ian Turner <ianturner88@gmail.com>
2018-06-22 21:59:21 -0300

    Add files via upload

--- solutions/PE025.cpp
+++ solutions/PE025.cpp
@@ -2,26 +2,29 @@
 #include<iostream>
 using namespace std;
 
-void Length_Calculator(int &n1, int &n2, int &length_difference, string &fibonacci1, string &fibonacci2, string &fibonacci_next);
+void Length_Calculator(int &n1, int &n2, int &n3, int &length_difference, string &fibonacci1, string &fibonacci2, string &fibonacci_next);
 void Fibonacci_Generator(string &fibonacci1, string &fibonacci2, string &fibonacci_next);
 void Fibonacci_Next(string &fibonacci_next);
 void Add_LSB(string &fibonacci1, string &fibonacci2, string &fibonacci_next, int &length_difference, int &carry, int &n1, int &n2, int &sum);
 void Add_MSB(string &fibonacci2, string &fibonacci_next, int &length_difference, int &n1, int &n2, int &carry);
 void Carry(string &fibonacci_next, int &carry);
 void ReverseFNext(string &fibonacci_next);
-void ReverseF1andF2(string &fibonacci1, string &fibonacci2);
+void ReverseAllFibonacciNumbers(string &fibonacci1, string &fibonacci2, string &fibonacci_next);
 
 int main()
 {
-	string fibonacci1 = "5", fibonacci2 = "8", fibonacci_next = "13";
-	int length_difference, carry = 0, count = 0, n1, n2, n3 = 1, sum = 0;
+	string fibonacci1 = "13", fibonacci2 = "21", fibonacci_next = "34";
+	int length_difference, carry = 0, count = 7, n1, n2, n3 = 1, sum = 0;
 	
+	ReverseAllFibonacciNumbers(fibonacci1, fibonacci2, fibonacci_next);
+	//Fibonacci_Generator(fibonacci1, fibonacci2, fibonacci_next);
+	//Fibonacci_Next(fibonacci_next);
+
 	while (n3 != 3)
 	{
 		Fibonacci_Generator(fibonacci1, fibonacci2, fibonacci_next);
-		ReverseF1andF2(fibonacci1, fibonacci2);
 		Fibonacci_Next(fibonacci_next);
-		Length_Calculator(n1, n2, length_difference, fibonacci1, fibonacci2, fibonacci_next);
+		Length_Calculator(n1, n2, n3, length_difference, fibonacci1, fibonacci2, fibonacci_next);
 		Add_LSB(fibonacci1, fibonacci2, fibonacci_next, length_difference, carry, n1, n2, sum);
 		Add_MSB(fibonacci2, fibonacci_next, length_difference, n1, n2, carry);
 		Carry(fibonacci_next, carry);
@@ -36,10 +39,14 @@ int main()
 	cout << "The " << count << " fibonacci number is " << fibonacci_next;
 }
 
-void Length_Calculator(int &n1, int &n2, int &length_difference, string &fibonacci1, string &fibonacci2, string &fibonacci_next)
+void Length_Calculator(int &n1, int &n2, int &n3, int &length_difference, string &fibonacci1, string &fibonacci2, string &fibonacci_next)
 {
+	/*Calculate the length of all strings. Needed for various subsequent for loops &
+	to determine if a Fibonacci number with 1000 digits has been found*/
+	
 	n1 = fibonacci1.length();
 	n2 = fibonacci2.length();
+	n3 = fibonacci_next.length();
 
 	length_difference = n2 - n1;
 }
@@ -54,6 +61,9 @@ void Fibonacci_Generator(string &fibonacci1, string &fibonacci2, string &fibonac
 
 void Fibonacci_Next(string &fibonacci_next)
 {
+	/*The ensuing functions calculate fibonacci_next.
+	The zero will be deleted later.*/
+	
 	string replacement = "0";
 
 	fibonacci_next = replacement;
@@ -77,6 +87,7 @@ void Add_LSB(string &fibonacci1, string &fibonacci2, string &fibonacci_next, int
 		}
 	}
 	
+	/*Delete the zero at the start of fibonacci_next*/
 	fibonacci_next.erase(fibonacci_next.begin());
 }
 
@@ -86,7 +97,7 @@ void Add_MSB(string &fibonacci2, string &fibonacci_next, int &length_difference,
 	
 	int remainder;
 	
-	for (int i = n2 - n1; i < n2; i++)
+	for (int i = n2 - n1 - 1; i >= 0; i--)
 	{
 		remainder = ((fibonacci2[i] - '0') + carry);
 		carry = remainder / 10;
@@ -97,6 +108,8 @@ void Add_MSB(string &fibonacci2, string &fibonacci_next, int &length_difference,
 
 void Carry(string &fibonacci_next, int &carry)
 {
+	/*For the most significant digit*/
+	
 	if (carry == 1)
 	{
 		fibonacci_next.push_back(carry + '0');
@@ -105,11 +118,17 @@ void Carry(string &fibonacci_next, int &carry)
 
 void ReverseFNext(string &fibonacci_next)
 {
+	/*Reverse the digit to the 'normal' order once the
+	algorithm has run.*/
+	
 	reverse(fibonacci_next.begin(), fibonacci_next.end());
 }
 
-void ReverseF1andF2(string &fibonacci1, string &fibonacci2)
+void ReverseAllFibonacciNumbers(string &fibonacci1, string &fibonacci2, string &fibonacci_next) 
 {
+	/*Reverses the digits of all numbers to all the algorithm to work*/
+	
 	reverse(fibonacci1.begin(), fibonacci1.end());
 	reverse(fibonacci2.begin(), fibonacci2.end());
+	reverse(fibonacci_next.begin(), fibonacci_next.end());
 }
