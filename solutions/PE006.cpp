f5c9d6eb9ca1c94530eee52d5da6defedce1281b
Ian Turner <ianturner88@gmail.com>
2018-06-27 16:04:13 -0300

    Update PE006.cpp

--- solutions/PE006.cpp
+++ solutions/PE006.cpp
@@ -4,22 +4,25 @@ using namespace std;
 
 void Sum_Squares(int &x, int &sum1);
 void Square_Sum(int &x, int &sum2);
-void Answer(int &answer, int &sum1, int &sum2);
+void Answer(long long int &answer, int &sum1, int &sum2);
 
 int main()
 {
-	int x = 0, sum1 = 0, sum2 = 0, answer = 0;
+	int x = 0, sum1 = 0, sum2 = 0; 
+	long long int answer = 0;
 	Sum_Squares(x, sum1);
 	x = 0;
 	Square_Sum(x, sum2);
 	Answer(answer, sum1, sum2);
 
-	cout << answer;
+	cout << answer << endl;
+
+	system("pause");
 }
 
 void Sum_Squares(int &x, int &sum1)
 {
-	while (x < 11)
+	while (x < 101)
 	{
 		sum1 += pow(x, 2);
 		x++;
@@ -28,7 +31,7 @@ void Sum_Squares(int &x, int &sum1)
 
 void Square_Sum(int &x, int &sum2)
 {
-	while (x < 11)
+	while (x < 101)
 	{
 		sum2 += x;
 		x++;
@@ -37,7 +40,7 @@ void Square_Sum(int &x, int &sum2)
 	sum2 = pow(sum2, 2);
 }
 
-void Answer(int &answer, int &sum1, int &sum2)
+void Answer(long long int &answer, int &sum1, int &sum2)
 {
 	answer = sum2 - sum1;
 }
