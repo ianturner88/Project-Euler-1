c058d576d1779b5cccbd29399129876fe1238ce7
Ian Turner <ianturner88@gmail.com>
2018-06-27 16:43:20 -0300

    Update PE007.cpp

--- solutions/PE007.cpp
+++ solutions/PE007.cpp
@@ -1,33 +1,54 @@
-#include <iostream>
+/* RTF: the 10001 prime number*/
+#include<iostream>
 using namespace std;
 
-int main (void)
-{
-  int big = 8, little = 2;
-  int count = 3;
-  int upperlimit = 4;
-  
-  cout << "Please enter nth prime you'd like to find" <<endl;
-  cin >> upperlimit;
+void Is_Prime(bool prime[], const int &upperlimit);
+void Answer(bool prime[], int &i);
 
-  while (count < upperlimit - 1)
-  {
-    big++;
-    count++;
-    little = 2;
-    while (little < ((big / 2) + 1))
-    {
-      if (big % little == 0)
-      {
-        big++;
-        little = 2;
-      }
-      
-      else 
-      {
-        little++;
-      }
-    }
-  }
-  cout << big;
+int main(void)
+{
+	/*Initialize the upperlimit; the testcase, n; the boolean array*/
+
+	const long long int upperlimit = 200000;
+	int answer = 0;
+	int i = 1;
+
+	bool prime[upperlimit];
+	memset(prime, true, sizeof(prime));
+
+	/*Identify all primes below 1 000 000*/
+	Is_Prime(prime, upperlimit);
+	Answer(prime, i);
+
+	cout << i - 2 << endl;
+
+	system("pause");
+}
+
+void Is_Prime(bool prime[], const int &upperlimit)
+{
+	/*Identify all primes below 1 000 000*/
+
+	for (int p = 2; p <= upperlimit; p++)
+	{
+		for (int i = 2 * p; i <= upperlimit; i += p)
+		{
+			prime[i] = false;
+		}
+	}
+}
+
+void Answer(bool prime[], int &i)
+{
+	int count = 0;
+
+	while (count < 10001)
+	{
+		if (prime[i] == true)
+		{
+			count++;
+		}
+
+		i += 2;
+	}
 }
