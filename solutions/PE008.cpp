9920364cd3b9da4425d558d609d08ab8d074e50d
Ian Turner <ianturner88@gmail.com>
2018-06-27 23:18:22 -0300

    Update PE008.cpp

--- solutions/PE008.cpp
+++ solutions/PE008.cpp
@@ -2,56 +2,50 @@
 #include <string>
 using namespace std;
 
-int main (void)
-{
-	int test;
-	
-	// determine how often the while loop needs to run
-	cout << "Please enter the number of tests" << endl;
-	cin >> test;
-	
-	int span;
-	string digits;
-	int long long best = 5;
-	int long long current = 0;
-	int start = 0;
-	int i;
-	int length;
+void User_Input(int &length, int &span, string &digits);
+void Calculations(int long long &current, int &i, int &length, int &span, string &digits, int long long &answer);
 
-	while (test--)
+int main(void)
+{
+	string digits;
+	int long long answer = 5, current = 0;
+	int start = 0, i = 0, length, span;
+
+	User_Input(length, span, digits);
+	Calculations(current, i, length, span, digits, answer);
+
+	cout << " " << endl << endl;
+	cout << answer << endl;
+
+	system("pause");
+}
+
+void Calculations(int long long &current, int &i, int &length, int &span, string &digits, int long long &answer)
+{
+	// start at slot 0 and go until we've come to the last 13 digits.
+	for (int start = 0; (start + span) < length; start++)
 	{
-		i = 1;
-		
-		// the number of digits to be read in
-		cout << "Please enter the string's length: " << endl;
-		cin >> length;
-		
-		//the number of consecutive digits to check
-		cout << "Please enter the span of digits to be tested: " << endl;
-		cin >> span;
-		
-		//the digits themselves... they can not be entered in line by line but must be read in as a single line
-		cout << "Please enter the digits to be investigated: " << endl;
-		cin >> digits;
-		
-		// start at slot 0 and go until we've come to the last 13 digits.
-		for (start = 0; (start + span) < length; start++)
+		current = 1;
+
+		//convert the digits from ASCII to numbers
+		for (i = 0; i < span; i++)
 		{
-			current = 1;
-			
-			//convert the digits from ASCII to numbers
-			for (i = 0; i < span; i++)
-			{
-				current *=	digits[start + i] - '0';
-			}
-			
-			// update the best 
-			if (current > best)
-			{
-				best = current;
-			}
+			current *= digits[start + i] - '0';
+		}
+
+		// update the best 
+		if (current > answer)
+		{
+			answer = current;
 		}
 	}
-	
-	cout << best;
+}
+
+void User_Input(int &length, int &span, string &digits)
+{
+	/*The program's input informaton is garnered*/
+
+	length = 1000;
+	span = 13;
+	digits = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
 }
