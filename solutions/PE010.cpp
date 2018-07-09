a1f5ca686fa313bacf582f5182465edd0eb89b01
Ian Turner <ianturner88@gmail.com>
2018-07-08 21:34:06 -0300

    Update PE010.cpp

--- solutions/PE010.cpp
+++ solutions/PE010.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
-#include <stdio.h>
-#include <string.h>
+#include<cstring>
 using namespace std;
 
 void Eratosthenes(const int &upperlimit, bool prime[]);
@@ -10,21 +9,22 @@ int main()
 {
 	const int upperlimit = 2000001;
 	long long int sum = 2;
-	string pause;
 	
 	bool prime[upperlimit];
 	memset(prime, true, sizeof(prime));
-	
+
 	Eratosthenes(upperlimit, prime);
 	Answer(sum, prime, upperlimit);
 
-	cout << "Sum is " << sum<<endl;
-	
+	cout << "Sum is " << sum << endl;
+
 	return 0;
 }
 
 void Eratosthenes(const int &upperlimit, bool prime[])
 {
+	/*Identify all prime numbers*/
+	
 	for (long long int p = 2; p <= upperlimit; p++)
 	{
 		for (long long int i = 2 * p; i <= upperlimit; i += p)
@@ -36,12 +36,17 @@ void Eratosthenes(const int &upperlimit, bool prime[])
 
 void Answer(long long int &sum, bool prime[], const int &upperlimit)
 {
+	/*Add all prime numbers to 'sum'*/
+	
+	int n = 3;
 
-	for (int n=3; n < upperlimit; n++)
+	while (n < upperlimit)
 	{
 		if (prime[n] == true)
 		{
 			sum += n;
 		}
+
+		n += 2;
 	}
 }
