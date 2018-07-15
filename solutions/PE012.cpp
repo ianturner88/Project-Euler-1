5b0dba21340c1ebb00f381a6b17839c7e27a3fb7
Ian Turner <ianturner88@gmail.com>
2018-07-14 21:02:04 -0300

    Update PE012.cpp

--- solutions/PE012.cpp
+++ solutions/PE012.cpp
@@ -1,31 +1,76 @@
-/// this code takes 14 hours to run. Hopefully, I find an improvement. 
 #include <iostream>
 #include <math.h>
 using namespace std;
 
-int main (void)
-{
-  int count = 0;
-  long long int little;
-  long long int triangle = 76576498;
-  long long int i = 1;
-  
-  while (count < 251)
-  {
-    count = 0;
-    i++;
-    triangle += i;
-    
-    for (little = 2; little <= sqrt(triangle); little++)
-    {
-      
-	  if (triangle%little == 0)
-      {
-        count++;
-      }
+void Triangle_Number_Generator(int &i, int &triangle_number);
+void Number_Divisor_Check(int &triangle_number, int &number_divisors, bool &condition);
+void Answer(int &triangle_number, bool &condition);
 
-    cout << "The count is: " << count << " , the little is: " << little << " and the triangle is: "<< triangle << endl;
- 	}
-  }
-  cout << "The " << i << "th triangle number --- " << triangle <<" --- is the first triangle number to have over 500 divisors.";
+int main()
+{
+	int i = 0, triangle_number = 0, number_divisors = 0, x; 
+	bool condition = true;
+
+	while (condition == true)
+	{
+		Triangle_Number_Generator(i, triangle_number);
+		Number_Divisor_Check(triangle_number, number_divisors, condition);
+		//Answer(triangle_number, condition);
+	}
+
+	cout << "Please enter input";
+	cin >> x;
+}
+
+void Triangle_Number_Generator(int &i, int &triangle_number)
+{
+	/*Generates triangle numbers*/
+	
+	i++;
+	triangle_number += i;
+}
+
+void Number_Divisor_Check(int &triangle_number, int &number_divisors, bool &condition)
+{
+	/*Counts the number of divisors of a given triangle number*/
+	
+	int upperlimit = pow(triangle_number, 0.5), divisor = 2;
+	number_divisors = 2;
+
+	if (triangle_number == 76576500)
+	{
+		int x = 0;
+	}
+
+	while (divisor < upperlimit)
+	{
+		if (triangle_number % divisor == 0)
+		{
+			number_divisors += 2;
+		}
+
+		divisor++;
+	}
+
+	/*We output the answer is there are more than 500 divisors.
+	Otherwise, we reset the number of divisors to zero.*/
+
+	if (number_divisors > 500)
+	{
+		Answer(triangle_number, condition);
+	}
+
+	else
+	{
+		number_divisors = 0;
+	}
+}
+
+void Answer(int &triangle_number, bool &condition)
+{
+	/*Prints the answer*/
+
+	cout << triangle_number << endl << endl;
+
+	condition = false;
 }
