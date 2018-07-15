8c7cc6b9856d6755a86a92b994bf7decdcbbf0b3
Ian Turner <ianturner88@gmail.com>
2018-07-15 13:02:10 -0300

    Update PE014.cpp

--- solutions/PE014.cpp
+++ solutions/PE014.cpp
@@ -1,41 +1,54 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-int main (void)
+void Sequence(int &n, int &i, int &k);
+void Storage(int collatz[], int &i, int &k, int &n);
+
+int main()
 {
-  int testnumber = 1;
-  int count;
-  int n;
-  int highestcount = 0;
-  int Collatz = 0;
-  
-  while (testnumber < 100)
-  {
-    testnumber++;
-    count = 1;
-    n = testnumber;
-  
-    while (n > 1)
-    {
-      if (n % 2 == 0)
-      {
-        n = n / 2;
-        count++;
-      }
-    
-      if (n % 2 == 1 && n!=1)
-      {
-        n = 3 * n + 1;
-        count++;
-      }
-    }
-  
-    if (count > highestcount)
-      {
-      highestcount = count;
-      Collatz = testnumber;
-      }
-  
-  cout << "Collatz: " << Collatz << " Highest Count: " << highestcount << " Number: "<< testnumber << " count: " << count << endl;
-  }
+	int n = 1, i = 0, k;
+	int collatz[10] = { 0 };
+
+	while (n < 1000000)
+	{
+		i++;
+		n = i;
+		n++;
+		Sequence(n, i, k);
+		Storage(collatz, i, k, n);
+	}
+}
+
+void Sequence(int &n, int &i, int &k)
+{
+	/*Calculates sequences for the given starting number
+	& breaks if the sequences starts calculating an already
+	determined route.*/
+	
+	k = 1;
+
+	while (n >= i && n != 1)
+	{
+		k++;
+		
+		if ((n % 2) == 0)
+		{
+			n = (n / 2);
+		}
+
+		else
+		{
+			n = 3 * n + 1;
+		}
+	}
+}
+
+void Storage(int collatz[], int &i, int &k, int &n)
+{
+	/*Every number's collatz sequence count is stored 
+	in the arry collatz[]*/
+
+	collatz[i] = collatz[n] + k;
+	int test = collatz[i];
 }
