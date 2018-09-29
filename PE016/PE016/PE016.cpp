fdeeb1c36047c9ca508f6818b95e97ba11624b08
Ian Turner <ianturner88@gmail.com>
2018-09-28 22:27:18 -0300

    generate & reverse functions are built

--- PE016/PE016/PE016.cpp
+++ PE016/PE016/PE016.cpp
@@ -3,13 +3,23 @@
 using namespace std;
 
 void Generate_Second_String(string &project_euler_0, string &project_euler_1);
+void Reverse_Strings(string &project_euler_0);
 
 int main(void)
 {
-	string project_euler_0 = "0", project_euler_1 = "";
+	string project_euler_0 = "0", project_euler_1;
+	int counter = 0;
 
 	//sets second variable equal to first
 	Generate_Second_String(project_euler_0, project_euler_1);
+
+	while (counter < 1000)
+	{
+
+	}
+
+	//reverses the number passed in
+	void Reverse_Strings(string &project_euler_0);
 
 	cout << project_euler_0 << endl;
 	cout << project_euler_1;
@@ -25,4 +35,12 @@ void Generate_Second_String(string &project_euler_0, string &project_euler_1)
 
 	//set 0 pe0 equal to pe1
 	project_euler_1 = project_euler_0;
-}g
+}
+
+void Reverse_Strings(string &project_euler_0, string &project_euler_1)
+{
+	/*Reverses the number passed in*/
+
+	//reverses project_euler_0
+	reverse(project_euler_0.begin(), project_euler_0.end());
+}
