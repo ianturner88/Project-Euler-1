55a696e952c59ab1b828abf2005b5039892de453
Ian Turner <ianturner88@gmail.com>
2018-12-18 12:02:01 -0500

    finished

---
+++ PE387/PE387/PE387.cpp
@@ -0,0 +1,130 @@
+//https://projecteuler.net/problem=387
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// initialize the function generating primes
+void Harshad(int n);
+
+int main(void)
+{
+	// we test the code for the case of Harshad numbers below 10000 where the sum of the strong, right truncatable Harshad primes is 90619
+	long long int n = 1000;
+
+	Harshad(n);
+}
+
+void Harshad(int n)
+{
+	long long int digitsum = 0;
+	long long int testcase;
+	int flag;
+	long long int HarshadPrime;
+	long long int dummytestcase;
+	int sum = 0;
+	int a, i, p, c, b;
+	int remainder;
+
+	bool prime[n + 1];
+	memset(prime, true, sizeof(prime));
+
+	// in this for loop, we make an array that identifies all numbers as either prime or not
+	for (long long int p = 2; p*p <= n; p++)
+	{
+		for (long long int i = 2 * p; i <= n; i += p)
+		{
+			prime[i] = false;
+		}
+	}
+
+	// here we check each slot. If the number is prime, we begin the algorithm required by the question
+	for (long long int p = 181; p <= n; p++)
+	{
+		flag = 0;
+
+		b = p;
+
+		// we check if the number is prime
+		if (prime[p] == true)
+		{
+			// if the number is prime, we hack off the right most digit
+			testcase = p / 10;
+			flag = 1;
+		}
+
+		// We need to check if the testcase divided by the sum of its digits is prime 
+		if (flag == 1)
+		{
+			// we set the sum of the digits to zero
+			digitsum = 0;
+
+			// we tabulate the sum of the digits
+			dummytestcase = testcase;
+			while (dummytestcase > 0)
+			{
+				digitsum += dummytestcase % 10;
+				dummytestcase = dummytestcase / 10;
+			}
+
+			// if the sum of digits evenly divides into the testcase, we set 'a' equal to the resulting number
+			if (testcase % digitsum == 0)
+			{
+				a = testcase / digitsum;
+			}
+
+			// if a is prime, the chase is on!
+			if (prime[a] == 1)
+			{
+				flag = 1;
+			}
+
+			else
+			{
+				flag = 0;
+			}
+
+			// we need to see if initial integer p is a Harshad number all the way through. 
+			while (flag == 1)
+			{
+				dummytestcase = testcase;
+				digitsum = 0;
+
+				while (dummytestcase > 0)
+				{
+					remainder = dummytestcase % 10;
+					digitsum += remainder;
+
+					dummytestcase = dummytestcase / 10;
+				}
+
+				if (testcase % digitsum == 0)
+				{
+					flag = 1;
+
+					c = testcase / digitsum;
+				}
+
+				else
+				{
+					flag = 0;
+				}
+
+				testcase = testcase / 10;
+
+				if (c == 1)
+				{
+					flag = 0;
+				}
+			}
+
+			if (c == 1)
+			{
+				sum += p;
+			}
+
+			c = 0;
+		}
+	}
+
+	cout << sum;
+}
