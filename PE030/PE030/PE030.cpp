e2d0005fe58608a428b001ca0389c8bc1e209c5e
Ian Turner <ianturner88@gmail.com>
2018-12-18 11:49:20 -0500

    finished

---
+++ PE030/PE030/PE030.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(void)
+{
+	int number = 1;
+	int remainder = 0;
+	int FourSum = 0;
+	int dummy = 0;
+	int dummysum = 0;
+	int count = 0;
+
+	while (number < pow(10, 5))
+	{
+		number++;
+		dummy = number;
+		remainder = 0;
+		dummysum = 0;
+
+		while (dummy != 0)
+		{
+			remainder = dummy % 10;
+			remainder = pow(remainder, 5);
+			dummysum += remainder;
+			dummy = dummy / 10;
+			//cout << "Number: " << number << "   Sum: " << dummysum << endl;
+		}
+
+		if (dummysum == number)
+		{
+			count++;
+			FourSum += dummysum;
+			cout << number << endl;
+		}
+	}
+
+	cout << "Foursum: " << FourSum << "   count: " << count;
+}
