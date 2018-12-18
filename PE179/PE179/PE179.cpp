c4482b489b683c23ad4f5133345d3b59bdf033be
Ian Turner <ianturner88@gmail.com>
2018-12-18 12:05:50 -0500

    finished... a faster method would use multiplication

---
+++ PE179/PE179/PE179.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(void)
+{
+	int n = 1;
+	int divisor;
+	int dc, pdc = 0;
+	float nsqrt;
+	int pc = 0;
+
+	while (n < pow(10, 7))
+	{
+		n++;
+
+		nsqrt = pow(n, 0.5);
+
+		divisor = 2;
+
+		dc = 2;
+
+		// checks the number of divisors of n
+		while (divisor < nsqrt)
+		{
+
+			if (n % divisor == 0)
+			{
+				dc += 2;
+			}
+
+			divisor++;
+		}
+
+		//for numbers like 9, this code double counts the case of 3, so 1 is subtracted. 
+		if (divisor == nsqrt)
+		{
+			dc--;
+		}
+
+		// checks if n & n + 1 have the same number of divisors
+		if (dc == pdc)
+		{
+			pc++;
+			//cout << "(" << n << ", " << n+1 << ")" << endl;
+		}
+
+		// stores the previous number's number of divisors!
+		pdc = dc;
+	}
+
+	cout << pc;
+
+	getchar();
+}
