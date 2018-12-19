aa544db6a461a18d69259838994b11fad8a070b9
Ian Turner <ianturner88@gmail.com>
2018-12-19 16:29:12 -0500

    touchups

--- PE013/PE013/PE013.cpp
+++ PE013/PE013/PE013.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 void Compare_Size(string &number_x, string &current_sum);
-void Calculate_Length_Two_Strings(string &number_x, int &number_x_length);
+void Calculate_Length_Smaller_String(string &number_x, int &number_x_length);
 void Reverse_2_Strings(string &number_x, string &current_sum);
-void LSB(string &number_x, string &current_sum, string PE013_answer, int &carry, int &number_x_length);
+void LSB(string &number_x, string &current_sum, string &PE013_answer, int &carry, int &number_x_length);
 
 int main(void)
 {
@@ -23,7 +23,7 @@ int main(void)
 		//ensures current sum is the larger variable
 		Compare_Size(number_x, current_sum);
 		//calculate the length of the 2 strings
-		Calculate_Length_Two_Strings(number_x, number_x_length);
+		Calculate_Length_Smaller_String(number_x, number_x_length);
 		//reverse the 2 strings
 		Reverse_2_Strings(number_x, current_sum);
 		//add the LSB of the 2 strings
@@ -34,7 +34,18 @@ int main(void)
 	getchar();
 }
 
-void LSB(string &number_x, string &current_sum, string PE013_answer, int &carry, int &number_x_length)
+void MSB(string &current_sum, int &number_x_length, int current_sum_length, 
+	string &PE013_answer, int &carry)
+{
+	/*add MSB of the larger string to PE013_answer*/
+
+	//the MSB digits that need to be added to PE013_answer
+	for (int i = 0; ; );
+
+
+}
+
+void LSB(string &number_x, string &current_sum, string &PE013_answer, int &carry, int &number_x_length)
 {
 	/*add the LSB of the 2 strings*/
 	
@@ -45,7 +56,12 @@ void LSB(string &number_x, string &current_sum, string PE013_answer, int &carry,
 	{
 		/*loop cycle through the full length of the smaller string*/
 		
+		//sum of the index i value of the 2 strings
 		int sum = ((number_x[i] - '0') + (current_sum[i] - '0') + carry);
+		//store the resulting sum in the variable PE013_answer
+		PE013_answer.push_back((sum % 10) + '0');
+		//if the sum exceeds 9, there's a carry
+		carry = sum / 10;
 	}
 }
 
@@ -56,7 +72,7 @@ void Reverse_2_Strings(string &number_x, string &current_sum)
 	reverse(current_sum.begin(), current_sum.end());
 }
 
-void Calculate_Length_Two_Strings(string &number_x, int &number_x_length)
+void Calculate_Length_Smaller_String(string &number_x, int &number_x_length)
 {
 	/*Calculate the length of the smaller string.
 	Used as the upperlimit when adding the LSB of the 2 numbers*/
