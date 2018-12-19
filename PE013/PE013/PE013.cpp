1fbb05ddf51a674c242d22efd833c4b93f7734af
Ian Turner <ianturner88@gmail.com>
2018-12-19 13:41:01 -0500

    testing the getline functionality

--- PE013/PE013/PE013.cpp
+++ PE013/PE013/PE013.cpp
@@ -6,5 +6,14 @@ using namespace std;
 
 int main(void)
 {
+	ifstream file("PE013.txt");
+	string line_x;
 
+	while (getline(file, line_x))
+	{
+		cout << line_x << endl;
+	}
+
+	//system pause
+	getchar();
 }
