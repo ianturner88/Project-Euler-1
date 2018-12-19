e018bbd1b72b99f8f1bcf872b1cb8f5e70981e0a
Ian Turner <ianturner88@gmail.com>
2018-12-19 15:12:29 -0500

    pass by reference implemented

--- PE013/PE013/PE013.cpp
+++ PE013/PE013/PE013.cpp
@@ -4,35 +4,41 @@
 #include<iostream>
 using namespace std;
 
-void Compare_Size(string number_x, string current_sum);
+void Compare_Size(string &number_x, string &current_sum);
+void Calculate_Length_Two_Strings(string &number_x, string &current_sum, int &number_x_length,
+	int &current_sum_length);
 
 int main(void)
 {
 	ifstream PE013_Input_File("PE013.txt");
 	string number_x, current_sum = "", PE013_answer = "";
-	int number_x_length, current_sum_length;
+	int number_x_length = 0, current_sum_length = 0;
 
 	while (getline(PE013_Input_File, number_x))
 	{
 		/*programs driving algorithm...
 		numbers are fed in one at a time*/
-
+		
 		//ensures current sum is the larger variable
 		Compare_Size(number_x, current_sum);
+		//calculate the length of the 2 strings
+		Calculate_Length_Two_Strings(number_x, current_sum, number_x_length, current_sum_length);
 	}
 
 	//system pause
 	getchar();
 }
 
-void Calculate_Length_Two_Strings(string number_x, string current_sum, int number_x_length, 
-	int current_sum_length)
+void Calculate_Length_Two_Strings(string &number_x, string &current_sum, int &number_x_length, 
+	int &current_sum_length)
 {
+	/*Calculate the length of the 2 strings*/
+	
 	number_x_length = number_x.length();
 	current_sum_length = current_sum.length();
 }
 
-void Compare_Size(string number_x, string current_sum)
+void Compare_Size(string &number_x, string &current_sum)
 {
 	/*compares the size of the two strings*/
 
