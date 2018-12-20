c65d8186e2dc3f0dfe82dd2f9d17925e1b8db8f8
Ian Turner <ianturner88@gmail.com>
2018-12-20 11:26:46 -0500

    using coding from PE013... changing architecture

--- PE016/PE016/PE016.cpp
+++ PE016/PE016/PE016.cpp
@@ -1,107 +1,156 @@
-#include<iostream>
 #include<string>
+#include<fstream>
+#include<streambuf>
+#include<iostream>
 using namespace std;
 
-void Generate_Second_String(string &pe0, string &pe1);
-void Reverse_Strings(string &pe0);
-void Add_LSB(string &pe0, string &pe1, string &answer, int &counter, int &carry);
-void Carry(string &answer, int &carry);
+void Compare_Size(string &number_x, string &current_sum);
+void Calculate_Length_Both_Strings(string &number_x, int &number_x_length, string &current_sum,
+	int &current_sum_length);
+void Reverse_2_Strings(string &number_x, string &current_sum);
+void LSB(string &number_x, string &current_sum, string &PE013_answer, int &carry, int &number_x_length);
+void MSB(string &current_sum, int number_x_length, int current_sum_length,
+	string &PE013_answer, int &carry);
+void Carry(string &PE013_anwer, int &carry);
+void Reset_Current_Sum_AND_PE013_answer(string &PE013_answer, string &current_sum);
+void Answer(string &PE013_answer, string &current_sum);
 
 int main(void)
 {
-	string pe0 = "1234", pe1, answer;
-	int exponent = 0, counter, carry = 0;
+	ifstream PE013_Input_File("PE013.txt");
+	string number_x, current_sum = "", PE013_answer = "";
+	int number_x_length = 0, carry = 0, current_sum_length = 0;
 
-	//reverses the numerical string
-	Reverse_Strings(pe0);
-
-	while (exponent < 15)
+	while (getline(PE013_Input_File, number_x))
 	{
-		/*the algorithm driving the program & 
-		the counter acts as the exponent*/
-		
-		//reset counter to start at new digit's LSB 
-		counter = 0;
+		/*programs driving algorithm...
+		numbers are fed in one at a time*/
 
-		//sets second variable pe1 equal to first
-		Generate_Second_String(pe0, pe1);
-		//add the least significant digits of the string
-		Add_LSB(pe0, pe1, answer, counter, carry);
-		//adds the carry if the MSB of LSB sum to greater than 9
-		Carry(answer, carry);
-		//sets pe0 equal to answer
-		Update_pe0(answer, pe0);
-
-		//the next power's sum will be determined
-		exponent++;
+		//ensures current sum is the larger variable
+		Compare_Size(number_x, current_sum);
+		//calculate the length of the 2 strings
+		Calculate_Length_Both_Strings(number_x, number_x_length, current_sum, current_sum_length);
+		//reverse the 2 strings
+		Reverse_2_Strings(number_x, current_sum);
+		//add the LSB of the 2 strings
+		LSB(number_x, current_sum, PE013_answer, carry, number_x_length);
+		//add the not summed digits of the longer string
+		MSB(current_sum, number_x_length, current_sum_length, PE013_answer, carry);
+		//add carry
+		Carry(PE013_answer, carry);
+		//update and clear the two variables
+		Reset_Current_Sum_AND_PE013_answer(PE013_answer, current_sum);
 	}
 
-	//reverses the number passed in
-	Reverse_Strings(pe0);
+	//outut the first 10 digits of the final sum
+	Answer(PE013_answer, current_sum);
 
-	cout << pe0 << endl;
-	cout << pe1 << endl;
-	cout << answer << endl;
-
-	int pause;
-	cin >> pause;
+	//system pause
+	getchar();
 }
 
-void Generate_Second_String(string &pe0, string &pe1)
+void Answer(string &PE013_answer, string &current_sum)
 {
-	/*Generates the second string, so that pe0 &
-	pe1 can be added together*/
+	/*the final answer requires only the first 10 digits of the final sum*/
 
-	//set 0 pe0 equal to pe1
-	pe1 = pe0;
+	PE013_answer = current_sum;
+
+	//output first 10 digits
+	for (int i = 0; i < 10; i++)
+	{
+		cout << PE013_answer[i];
+	}
 }
 
-void Reverse_Strings(string &pe0)
+void Reset_Current_Sum_AND_PE013_answer(string &PE013_answer, string &current_sum)
 {
-	/*Reverses the number passed in*/
+	/*at the end of one cycle, these two variables are reset*/
 
-	//reverses pe0
-	reverse(pe0.begin(), pe0.end());
+	//PE013_answer is used to store the sum of current_sum & number_x
+	current_sum = PE013_answer;
+	//current_sum needs to be reset since it'll be reversed at the start of the cycle again
+	reverse(current_sum.begin(), current_sum.end());
+	//clear this variable so a new cycle may begin
+	PE013_answer = "";
 }
 
-void Add_LSB(string &pe0, string &pe1, string &answer, int &counter, int &carry)
+void Carry(string &PE013_anwer, int &carry)
 {
-	//used to store the result of pe0 & pe1
-	int sum;
+	/*add the final hierarchical digit, if the digit exists*/
 
-	//test
-	int test = (pe0[counter] - '0');
-	int test1 = (pe1[counter] - '0');
-	//
-
-	//adds to the 'counter' digit of the two strings 
-	sum = (pe0[counter] - '0') + (pe1[counter] - '0');
-
-	//adds the LSB to the answer
-	answer.push_back(sum % 10 + '0');
-
-	//if the sum exceeds 9, there is a carry
-	carry = sum / 10;
-}
-
-void Carry(string &answer, int &carry)
-{
-	/*if the last digits summed from the LSB function
-	sum to greater than 9, this function is called*/
-	
 	if (carry == 1)
 	{
-		/*last digits summed of LSB greater than 9*/
-		
-		//the carry is added to answer
-		answer.push_back(carry + '0');
+		PE013_anwer.push_back(carry + '0');
 	}
 }
 
-void Update_pe0(string &answer, string &pe0)
+void MSB(string &current_sum, int number_x_length, int current_sum_length,
+	string &PE013_answer, int &carry)
 {
-	/*function updates the values pe0, so that
-	the next power of 2 may be found*/
+	/*add MSB of the larger string to PE013_answer*/
 
-	pe0 = answer;
+	//the MSB digits that need to be added to PE013_answer
+	for (int i = number_x_length; i < current_sum_length; i++)
+	{
+		//test
+		i;
+		int check = (current_sum[i] - '0');
+		carry;
+
+		//sum remaining digits
+		int sum = (current_sum[i] - '0') + carry;
+		//store sum in answer
+		PE013_answer.push_back((sum % 10) + '0');
+
+		//check if sum exceeds 9
+		carry = sum / 10;
+	}
+}
+
+void LSB(string &number_x, string &current_sum, string &PE013_answer, int &carry, int &number_x_length)
+{
+	/*add the LSB of the 2 strings*/
+
+	//reset carry variable
+	carry = 0;
+
+	for (int i = 0; i < number_x_length; i++)
+	{
+		/*loop cycle through the full length of the smaller string*/
+
+		//sum of the index i value of the 2 strings
+		int sum = ((number_x[i] - '0') + (current_sum[i] - '0') + carry);
+		//store the resulting sum in the variable PE013_answer
+		PE013_answer.push_back((sum % 10) + '0');
+		//if the sum exceeds 9, there's a carry
+		carry = sum / 10;
+	}
+}
+
+void Reverse_2_Strings(string &number_x, string &current_sum)
+{
+	/*reverse the 2 strings*/
+	reverse(number_x.begin(), number_x.end());
+	reverse(current_sum.begin(), current_sum.end());
+}
+
+void Calculate_Length_Both_Strings(string &number_x, int &number_x_length, string &current_sum,
+	int &current_sum_length)
+{
+	/*Calculate the length of the smaller string.
+	Used as the upperlimit when adding the LSB of the 2 numbers*/
+
+	current_sum_length = current_sum.length();
+	number_x_length = number_x.length();
+}
+
+void Compare_Size(string &number_x, string &current_sum)
+{
+	/*compares the size of the two strings*/
+
+	if (number_x.length() > current_sum.length())
+	{
+		//need to ensure the second variable is always the larger number
+		swap(current_sum, number_x);
+	}
 }
