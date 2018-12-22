251f51fd594d77fc1cad1dd34c92a2fe7e22aaf5
Ian Turner <ianturner88@gmail.com>
2018-12-21 19:23:12 -0500

    learning the fundamentals

---
+++ ReferencevsPass/ReferencevsPass/ReferencevsPass.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void Add(int &a, int &b);
+
+int main(void)
+{
+	int x = 5, y = 6;
+
+	Add(x, y);
+
+	cout << x << endl;
+	cout << y << endl << endl;
+
+	getchar();
+}
+
+void Add(int &a, int &b)
+{
+	cout << a << endl;
+	cout << b << endl;
+
+	a -= 2;
+	b -= 2;
+	
+	cout << a << endl;
+	cout << b << endl << endl;
+}
