8ad052a1e11074c8c64b2d3befad0cc2aa3193b8
Ian Turner <ianturner88@gmail.com>
2019-02-16 01:11:32 -0400

    vector range issues

--- PE052/PE052/PE052.cpp
+++ PE052/PE052/PE052.cpp
@@ -1,45 +1,77 @@
 #include<iostream>
 #include <vector>
 #include <array>
+//#include <algorithm>
 using namespace std;
 
-void parse_six_digit_number(int six_digit_number, int number_counter, int zero_through_nine[]);
-void store_fingerprint(int number_counter, int zero_through_nine[], vector<string> &fingerprint);
+void Parse_Six_Digit_Number(int six_digit_number, int number_counter, int zero_through_nine[]);
+int Create_Fingerprint(int zero_through_nine[], int power_10[]);
+void Power_10_Array(int power_10[]);
+void Store_Fingerprint(int fingerprint_int, string fingerprint_string, vector<string> &fingerprint, int number_counter);
 
 int main(void)
 {
-	int six_digit_number = 122555, number_counter = 0, zero_through_nine[10] = { 0 };
+	int six_digit_number = 567567, number_counter = 0, zero_through_nine[10] = { 0 }, power_10[6], fingerprint_int;
+	string fingerprint_string = "";
 	vector<string> fingerprint;
+
+	Power_10_Array(power_10);
 
 	while (six_digit_number < 1000000)
 	{
-		parse_six_digit_number(six_digit_number, number_counter, zero_through_nine);
-		store_fingerprint(number_counter, zero_through_nine, fingerprint);
+		Parse_Six_Digit_Number(six_digit_number, number_counter, zero_through_nine);
+		fingerprint_int = Create_Fingerprint(zero_through_nine, power_10);
+		Store_Fingerprint(fingerprint_int, fingerprint_string, fingerprint, number_counter);
 	}
 }
 
-void store_fingerprint(int number_counter, int zero_through_nine[], vector<string> &fingerprint)
+void Store_Fingerprint(int fingerprint_int, string fingerprint_string, vector<string> &fingerprint, int number_counter)
+{
+	/*converts fingerprint int to a 10 digit string*/
+	int remainder;
+
+	while (fingerprint_int > 0)
+	{
+		//identify least significant digit of fingerprint_int
+		remainder = fingerprint_int % 10;
+		//shrink number
+		fingerprint_int /= 10;
+		//create string length
+		fingerprint_string += (remainder + '0');
+	}
+
+	//store the individual number's fingerprint 
+	fingerprint[number_counter] = fingerprint_string;
+}
+
+int Create_Fingerprint(int zero_through_nine[], int power_10[])
 {
 	/*create & store all individual numbers' fingerprints*/
 	
-	string fingerprint_to_store = "";
+	int fingerprint_to_store = 0, power_counter = 0;
 
-	for (int i = 0; i < sizeof(zero_through_nine); i++)
+	for (int i = 0; i < 11; i++)
 	{
 		// final output will be in form abcdefghij (a = 0, ... j = 9)
-		fingerprint_to_store += zero_through_nine[i];
+		fingerprint_to_store += (zero_through_nine[i] * power_10[power_counter]);
+		//icrease power of 10
+		power_counter++;
 	}
 
-	//store fingerprint
-	fingerprint[number_counter] = fingerprint_to_store;
+	return fingerprint_to_store;
 }
 
-void parse_six_digit_number(int six_digit_number, int number_counter, int zero_through_nine[])
+void Parse_Six_Digit_Number(int six_digit_number, int number_counter, int zero_through_nine[])
 {
 	/*create a fingprint for every number that is 6 digits long*/
 	
 	int least_significant_digit;
-	zero_through_nine[10] = { 0 };
+	zero_through_nine[10];
+
+	for (int i = 0; i < 11; i++)
+	{
+		zero_through_nine[i] = 0;
+	}
 	
 	while (six_digit_number > 0)
 	{
@@ -49,8 +81,15 @@ void parse_six_digit_number(int six_digit_number, int number_counter, int zero_t
 		least_significant_digit = six_digit_number % 10;
 		//downsize number by one digit
 		six_digit_number /= 10;
-
 		//tally up the given number's fingerprint
 		zero_through_nine[least_significant_digit] += 1;
+	}
+}
+
+void Power_10_Array(int power_10[])
+{
+	for (int i = 0; i < 11; i++)
+	{
+		power_10[i] = pow(10, i);
 	}
 }
