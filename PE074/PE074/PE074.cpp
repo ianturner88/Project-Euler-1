37bf692f71b51f6b56084557bd95298dfa6d5566
Ian Turner <ianturner88@gmail.com>
2019-04-07 23:03:50 -0300

    implement malloc

--- PE074/PE074/PE074.cpp
+++ PE074/PE074/PE074.cpp
@@ -2,22 +2,49 @@
 #include <vector>
 
 #include "ProjectEulerLibrary.h"
+#include "PE074.h"
+
+enum PE074
+{
+	UPPERLIMIT = 1000000
+};
 
 int main(void)
 {
-	std::cout << "Ian" << std::endl;
-
 	std::vector<int> digit_factorial_chains;
-	int n = 9;
+	int digit_factorial_upper_limit = 10, numbers_factorial_sum[100000] = { 0 }, number = 145,
+		numbers_digits_factorial_sum;
+	bool state_machine[100000] = { false };
 
-	Euler::Digit_Factorial_Chains(n, digit_factorial_chains);
+	//calculate the factorials of 1 to 9
+	Euler::Digit_Factorial_Chains(digit_factorial_upper_limit, digit_factorial_chains);
 
-	for (int i = 1; i < n; i++)
+	while (number < UPPERLIMIT)
 	{
-		int test = digit_factorial_chains[i];
-		
-		std::cout << digit_factorial_chains[i- 1] << std::endl;
+		//find the sum of every number's digits' factorials
+		numbers_digits_factorial_sum = Numbers_Digit_Factorial_Sum(digit_factorial_chains, number);
+		//memoization
+		numbers_factorial_sum[number] = numbers_digits_factorial_sum;
 	}
 
 	getchar();
+}
+
+int Numbers_Digit_Factorial_Sum(std::vector<int> digit_factorial_chains, int number)
+{
+	/*find the sum of the factorial of the numbers digits*/
+	
+	int digit, digit_factorial_sum = 0, number;
+
+	while (number > 0)
+	{
+		//identify nth digit of number
+		digit = number % 10;
+		//shrink number
+		number /= 10;
+		//sum of the digit's factorial 
+		digit_factorial_sum += digit_factorial_chains[digit - 1];
+	}
+
+	return digit_factorial_sum;
 }
---
+++ PE074/PE074/PE074.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include<vector>
+
+int Numbers_Digit_Factorial_Sum(std::vector<int> digit_factorial_chains, int number);
