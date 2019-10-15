269a81564f869a80e9749d5817f9191fa6b1c3d7
Ian Turner <ianturner88@gmail.com>
2019-10-14 21:26:03 -0400

    sorting vector proof of concept confirmed

--- Sorting_Vector_Example/Sorting_Vector_Example/Sorting_Vector_Example.cpp
+++ Sorting_Vector_Example/Sorting_Vector_Example/Sorting_Vector_Example.cpp
@@ -1,29 +1,48 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
+
+void Print(std::vector<int> example);
+void Add_Contents(std::vector<int>& example, int multiplier);
 
 int main(void)
 {
 	std::vector<int> example;
 
+	std::cout << "Round 1: " << std::endl;
+
+	//round 1
+	Add_Contents(example, 4);
+	Print(example);
+
+	std::cout << "Round 2: " << std::endl;
+
+	//round 2
+	Add_Contents(example, 1);
+	Print(example);
+
+	std::cout << "Round 3: " << std::endl;
+
+	std::sort(example.begin(), example.end());
+	Print(example);
+}
+
+void Print(std::vector<int> example)
+{
+	//print vector contents
+	
+	for (int i = 0; i < example.size(); i++)
+	{
+		std::cout << example[i] << std::endl;
+	}
+}
+
+void Add_Contents(std::vector<int>& example, int multiplier)
+{
+	//add numbers to the vector
 	for (int i = 0; i < 5; i++)
 	{
 		//store large numbers
-		example.push_back(i*4);
-	}
-
-	for (int i = 0; i < 5; i++)
-	{
-		std::cout << example[i] << std::endl;
-	}
-
-	for (int i = 0; i < 5; i++)
-	{
-		//store small numbers
-		example.push_back(i);
-	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		std::cout << example[i] << std::endl;
+		example.push_back(i * multiplier);
 	}
 }
