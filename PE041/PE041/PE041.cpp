bcd78a58c467f4e85a14df262eddddae7badb5f1
Ian Turner <ianturner88@gmail.com>
2020-04-07 17:17:44 -0400

    Sieve of Eratosthenes built

--- PE041/PE041/PE041.cpp
+++ PE041/PE041/PE041.cpp
@@ -2,37 +2,53 @@
 #include <iostream>
 
 std::vector<int> SieveOfEratosthenes();
+std::vector<int> prime_sieve;
+const long long int upperlimit = 7654321;
+bool prime[upperlimit] = { 0 };
+unsigned long long int prime_number;
 
 int main(void) {
-	const long long int upperlimit = 9999999;
-	bool prime[upperlimit] = { 0 };
-	std::vector<int> prime_sieve;
-	int prime_number;
 
+	std::vector<int> prime_numbers = SieveOfEratosthenes();
+
+	std::cout << "Toronto";
+
+	getchar();
+}
+
+bool is_pandigital(int input_number)
+{
+	/*determines if a number is pandigital*/
+
+	return 0;
+}
+
+
+std::vector<int> SieveOfEratosthenes()
+{
+	/*returns a vector of all prime numbers less than the specified upperlimit*/
 	for (int i = 2; i * i <= upperlimit; i++)
 	{
 		if (prime[i] == false)
+		//the number is prime
 		{
-			for (int multiple = i * i; multiple <= upperlimit; multiple += i)
+			for (unsigned long long int multiple = i * i; multiple <= upperlimit; multiple += i)
 			{
+				//mark all subsequent multiples as non-prime
 				prime[multiple] = true;
 			}
 		}
 	}
 
-	for (int i = 0; i <= upperlimit; i++)
+	for (unsigned long long int i = 0; i <= upperlimit; i++)
 	{
 		if (prime[i] == false)
+		//extract all prime numbers into a new vector
 		{
 			prime_number = i;
 			prime_sieve.push_back(prime_number);
 		}
 	}
 
-	for (int i = 2; i < prime_sieve.size(); i++)
-	{
-		std::cout << prime_sieve[i] << std::endl;
-	}
-
-	getchar();
+	return prime_sieve;
 }
