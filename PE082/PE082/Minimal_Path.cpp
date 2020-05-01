9eccb28a4b9d0cb79e48fbb5e061be56f02b304e
Ian Turner <ianturner88@gmail.com>
2020-05-01 11:21:08 -0400

    rebuild cheapest path function

--- PE082/PE082/Minimal_Path.cpp
+++ PE082/PE082/Minimal_Path.cpp
@@ -27,12 +27,12 @@ int Minimal_Path_Sum(PE082_Matrix pe082_matrix[NxN][NxN])
 
 		for (int j = row; j < (NxN - 2); j++)
 		{
-			Downward_Path(pe082_matrix, row, column);
+			Downward_Path(pe082_matrix, j, column);
 		}
 
 		for (int j = (NxN - 1); j > 1; j--)
 		{
-			Upward_Path(pe082_matrix, row, column);
+			Upward_Path(pe082_matrix, j, column);
 		}
 
 		for (int j = row; j < (NxN - 1); j++)
@@ -43,64 +43,57 @@ int Minimal_Path_Sum(PE082_Matrix pe082_matrix[NxN][NxN])
 
 	/* the cheapest path from the top left corner
 	of the matrix to bottom right corner of the matrix */
-	return pe082_matrix[(NxN - 1)][(NxN - 1)].cheapest_sum;
+	return pe082_matrix[(NxN - 1)][(NxN - 1)].cheapest_downwards_sum;
 }
 
 void Upward_Path(PE082_Matrix pe082_matrix[NxN][NxN], int row, int column)
 {
 	//calculates the sum in the right-ward direction
-	int	temp_sum = pe082_matrix[row][column].cheapest_sum +
-		pe082_matrix[(row + 1)][column].element_value;
+	int	temp_sum = pe082_matrix[row][column].cheapest_upwards_sum +
+		pe082_matrix[(row - 1)][column].element_value;
 
-	if ((temp_sum < pe082_matrix[row][column].cheapest_sum) ||
-		(pe082_matrix[row][(column + 1)].is_untouched == true))
+	if ((temp_sum < pe082_matrix[row][column].cheapest_upwards_sum) ||
+		(pe082_matrix[(row - 1)][column].is_untouched == true))
 	{
 		//the new sum is cheaper OR the element has not yet been touched
-		pe082_matrix[row][column].cheapest_sum = temp_sum;
+		pe082_matrix[row][column].cheapest_upwards_sum = temp_sum;
 		//stores the cheapest path to the current element
-		pe082_matrix[row][column].cheapest_path = Cheapest_Path(pe082_matrix, row, column);
+		pe082_matrix[row][column].cheapest_upwards_path = Cheapest_Path(pe082_matrix, row, column);
 	}
-
-	// indicates the element has alread been travesered at least once
-	pe082_matrix[row][column].is_untouched = true;
 }
 
 void Downward_Path(PE082_Matrix pe082_matrix[NxN][NxN], int row, int column)
 {
 	//calculates the sum in the right-ward direction
-	int	temp_sum = pe082_matrix[row][column].cheapest_sum +
+	int	temp_sum = pe082_matrix[row][column].cheapest_downwards_sum +
 		pe082_matrix[(row + 1)][column].element_value;
 
-	if ((temp_sum < pe082_matrix[row][column].cheapest_sum) ||
-		(pe082_matrix[row][(column + 1)].is_untouched == true))
+	if ((temp_sum < pe082_matrix[(row + 1)][column].cheapest_downwards_sum) ||
+		(pe082_matrix[(row + 1)][column].is_untouched == true))
 	{
 		//the new sum is cheaper OR the element has not yet been touched
-		pe082_matrix[row][column].cheapest_sum = temp_sum;
+		pe082_matrix[(row + 1)][column].cheapest_downwards_sum = temp_sum;
 		//stores the cheapest path to the current element
-		pe082_matrix[row][column].cheapest_path = Cheapest_Path(pe082_matrix, row, column);
+		pe082_matrix[(row + 1)][column].cheapest_downwards_path = Cheapest_Path(pe082_matrix, row, column);
 	}
-
-	// indicates the element has alread been travesered at least once
-	pe082_matrix[row][column].is_untouched = true;
 }
 
 void Rightward_Path(PE082_Matrix pe082_matrix[NxN][NxN], int row, int column)
 {
 	//calculates the sum in the right-ward direction
-	int	temp_sum = pe082_matrix[row][column].cheapest_sum +
+	int	temp_sum = pe082_matrix[row][column].cheapest_downwards_sum +
 		pe082_matrix[row][(column + 1)].element_value;
 
-	if ((temp_sum < pe082_matrix[row][column].cheapest_sum) ||
+	if ((temp_sum < pe082_matrix[row][(column + 1)].cheapest_downwards_sum) ||
 		(pe082_matrix[row][(column + 1)].is_untouched == true))
 	{
 		//the new sum is cheaper OR the element has not yet been touched
-		pe082_matrix[row][column].cheapest_sum = temp_sum;
+		pe082_matrix[row][(column + 1)].cheapest_rightward_sum = temp_sum;
 		//stores the cheapest path to the current element
-		pe082_matrix[row][column].cheapest_path = Cheapest_Path(pe082_matrix, row, column);
+		pe082_matrix[row][(column + 1)].cheapest_rightwards_path = Cheapest_Path(pe082_matrix, row, column);
+		//indicates the default cheapest sum of 0 has been altered
+		pe082_matrix[row][(column + 1)].is_untouched = false;
 	}
-
-	// indicates the element has alread been travesered at least once
-	pe082_matrix[row][column].is_untouched = true;
 }
 
 std::vector<int> Cheapest_Path(PE082_Matrix pe082_matrix[NxN][NxN], int row, int column)
--- PE082/PE082/PE082.h
+++ PE082/PE082/PE082.h
@@ -11,19 +11,34 @@ class PE082_Matrix
 public:
 	// the value of the matrix at the given point
 	int element_value;
-	// the value of the shortest path to the given point
+
+	// the value of the shortest path to the given point in the downward direction
+	int cheapest_downwards_sum = 0;
+	// the value of the shortest path to the given point in the upward direction
+	int cheapest_upwards_sum = 0;
+	// the cheapest path to traverse the matrix rightwards
+	int cheapest_rightward_sum = 0;
+	// the overall cheapest sum
 	int cheapest_sum = 0;
+
 	/* indicates if the shortest path to that point has yet to investigated ->
 	since the cheapest sum is initialized to 0, this variable is needed to indicate
 	that 0 not the cheapest path*/
 	bool is_untouched = true;
-	// contains the cheapest path from the right-side to the left-side of the matrix
+
+	// contains the cheapest path down a column
+	std::vector<int> cheapest_downwards_path;
+	// contains the cheapest path up a column
+	std::vector<int> cheapest_upwards_path;
+	// contains the cheapest path rightwards across a column
+	std::vector<int> cheapest_rightwards_path;
+	// the cheapest path
 	std::vector<int> cheapest_path;
 
 	// sets the value of the matrix at a given point
 	void set_Indice(int input_number) { element_value = input_number; }
 	// sets initializes the cheapest path with every possible starting point
-	void set_Cheapest_Sum(int input_number) { cheapest_sum = input_number; }
+	void set_Cheapest_Sum(int input_number) { cheapest_downwards_sum = input_number; }
 	// returns the value of the matrix at a given point
 	int get_Indice() { return element_value; }
 };
